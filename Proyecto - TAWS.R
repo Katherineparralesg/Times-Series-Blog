690df083d293c2c632e4e5fe0aae6485d60f208b
Katherine Parrales <35158181+Katherineparralesg@users.noreply.github.com>
2019-07-24 13:40:53 -0500

    Update Proyecto - TAWS.R

--- Proyecto - TAWS.R
+++ Proyecto - TAWS.R
@@ -1,3 +1,5 @@
+library(tseries)
+
 #Through this function the databases are enabled: Unemployment_female / Unemployment_male
 attach(unemployment_female)
 attach(unemployment_male)
@@ -49,38 +51,99 @@ plot(unemployment_male_desc, xlab = "Year", col = "royalblue1")
 
 #Transformation Of the serie
 #Stabilization of variance
-unemployment_female_stab <- log(unemployment_female_ts)
-unemployment_male_stab <- log(unemployment_male_ts)
-plot(unemployment_female_stab, xlab = "Year", ylab = "Unemployment Female", col = "lightsalmon", main = "Stabilization of variance")
-plot(unemployment_male_stab, xlab = "Year", ylab = "Unemployment Male", col = "royalblue1", main = "Stabilization of variance")
+unemployment_female_log <- log(unemployment_female_ts)
+unemployment_male_log <- log(unemployment_male_ts)
+plot(unemployment_female_log, xlab = "Year", ylab = "Unemployment Female", col = "lightsalmon", main = "Stabilization of variance")
+plot(unemployment_male_log, xlab = "Year", ylab = "Unemployment Male", col = "royalblue1", main = "Stabilization of variance")
+
+#Dickey-Fuller Test (1)
+adf.test(unemployment_female_log,k = 12)
+adf.test(unemployment_male_log,k = 12)
+#As we can see, the p-values are greater than the level of significance (p>0.05), so the conclusion 
+#is that the series are not stationary one.
 
 #Trend elimination
-unemployment_female_trendelim <- diff(unemployment_female_stab)
-unemployment_male_trendelim <- diff(unemployment_male_stab)
-plot(unemployment_female_trendelim, xlab = "Year", ylab = "Unemployment Female", col = "lightsalmon", main = "Trend elimination")
-plot(unemployment_male_trendelim, xlab = "Year", ylab = "Unemployment Male", col = "royalblue1", main = "Trend elimination")
+unemployment_female_diff <- diff(unemployment_female_log)
+unemployment_male_diff <- diff(unemployment_male_log)
+plot(unemployment_female_diff, xlab = "Year", ylab = "Unemployment Female", col = "lightsalmon", main = "Trend elimination")
+plot(unemployment_male_diff, xlab = "Year", ylab = "Unemployment Male", col = "royalblue1", main = "Trend elimination")
+
+
+#Dickey-Fuller Test (2)
+adf.test(unemployment_female_diff,k = 12)
+adf.test(unemployment_male_diff,k = 12)
+#As we can see, the p-values are greater than the level of significance (p>0.05), so the conclusion 
+#is that the series are not integrated in order one I(1)
 
 # Elimination of seasonality
-unemployment_female_seasonelim <- diff(unemployment_female_trendelim, lag = 12)
-unemployment_male_seasonelim <- diff(unemployment_male_trendelim, lag = 12)
-plot(unemployment_female_seasonelim,  xlab = "Year", ylab = "Unemployment Female", col = "lightsalmon", main = "Elimination of seasonality")
-plot(unemployment_male_seasonelim, xlab = "Year", ylab = "Unemployment Male", col = "royalblue1", main = "Elimination of seasonality")
+unemployment_female_diff2 <- diff(unemployment_female_diff)
+unemployment_male_diff2 <- diff(unemployment_male_diff)
+plot(unemployment_female_diff2, xlab = "Year", ylab = "Unemployment Female", col = "lightsalmon", main = "Elimination of seasonality")
+plot(unemployment_male_diff2, xlab = "Year", ylab = "Unemployment Male", col = "royalblue1", main = "Elimination of seasonality")
 
+#Dickey-Fuller Test (3)
+adf.test(unemployment_female_diff2,k = 12)
+adf.test(unemployment_male_diff2,k = 12)
+#As we can see, the p-value is smaller than the level of significance (p<0.05), so the conclusion 
+#is that the series are integrated in order one I(2)
 
-#Dickey-Fuller Test
-library(tseries)
-#adf.test(unemployment_female_ts)
-#adf.test(unemployment_male_ts)
-
-
-#Autocorrelation function estimation
+#Autocorrelation function estimation & Partial Autocorrelation function estimation
 library(astsa)
-Acf(unemployment_female_ts, plot = TRUE, main = "ACF - Unemployment Female")
-Acf(unemployment_male_ts, plot = TRUE, main = "ACF - Unemployment Male")
-
-#Partial Autocorrelation function estimation
-Pacf(unemployment_female_ts, plot = TRUE, main = "PACF - Unemployment Female" )
-Pacf(unemployment_male_ts, plot = TRUE, main = "PACF - Unemployment Male" )
+acf2(unemployment_female_diff2, plot = TRUE, main = "ACF & PACF - Unemployment Female")
 
 #Cross-Correlation Function Estimation
-Ccf(unemployment_female_ts,unemployment_male_ts, plot = TRUE, main = "CCF - Unemployment Female & Unemployment Male")
+ccf(unemployment_female_diff2,unemployment_male_log, plot = TRUE, main = "CCF - Unemployment Female & Unemployment Male")
+
+# ARIMA MODEL 
+# AR 
+ar(unemployment_female_diff2)
+
+# Stimation
+# AR
+simf1<-arima.sim(list(ar=c(-0.5)),n=200,innov=unemployment_female_log)
+simf2<-arima.sim(list(ar=c(.5,-0.5)),n=200,innov=unemployment_female_log)
+simf3<-arima.sim(list(ar=c(.2,-.4,.6)),n=200,innov=unemployment_female_log)
+simf4<-arima.sim(list(ar=c(.5,.4,-.6,.4)),n=200,innov=unemployment_female_log)
+
+ts.plot(simf1)
+ts.plot(simf2)
+ts.plot(simf3)
+ts.plot(simf4)
+
+acf2(simf1, plot = TRUE, main = "ACF & PACF - Unemployment Female Simf1")
+acf2(simf2, plot = TRUE, main = "ACF & PACF - Unemployment Female Simf2") 
+acf2(simf3, plot = TRUE, main = "ACF & PACF - Unemployment Female Simf3")
+acf2(simf4, plot = TRUE, main = "ACF & PACF - Unemployment Female Simf4")
+
+arima(simf1)
+arima(simf2)
+arima(simf3)
+arima(simf4)
+
+# MA
+simff1<-arima.sim(list(ma=c(1)),n=200,innov=unemployment_female_log)
+simff2<-arima.sim(list(ma=c(1,-1)),n=200,innov=unemployment_female_log)
+simff3<-arima.sim(list(ma=c(1,1,1)),n=200,innov=unemployment_female_log)
+simff4<-arima.sim(list(ma=c(1/4,1/4,1/4,1/4)),n=200,innov=unemployment_female_log)
+
+ts.plot(simff1)
+ts.plot(simff2)
+ts.plot(simff3)
+ts.plot(simff4)
+
+acf2(simff1, plot = TRUE, main = "ACF & PACF - Unemployment Female Simff1")
+acf2(simff2, plot = TRUE, main = "ACF & PACF - Unemployment Female Simff2") 
+acf2(simff3, plot = TRUE, main = "ACF & PACF - Unemployment Female Simff3")
+acf2(simff4, plot = TRUE, main = "ACF & PACF - Unemployment Female Simff4")
+
+arima(simff1)
+arima(simff2)
+arima(simff3)
+arima(simff4)
+
+# ARIMA
+arima(unemployment_female_diff2)
+arima(unemployment_male_log)
+
+arima.sim(unemployment_female_diff2)
+arima.sim(unemployment_male_log)
